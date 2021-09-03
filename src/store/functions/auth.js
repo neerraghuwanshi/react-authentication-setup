206702d9c73e196eeeb6496f8d5413e5b9b34545
Neer Raghuwanshi <raghuwanshineer1@gmail.com>
2021-09-03 15:50:35 +0530

    Removed a console.log() statement from setInitialAuthState function in  /src/store/functions/auth

--- src/store/functions/auth.js
+++ src/store/functions/auth.js
@@ -32,7 +32,6 @@ export const setInitialAuthState = () => {
             ))
         } 
         catch (error) {
-            console.log(error.message)
             dispatch(authLogout())
         }
     }
